3b9edf5064f4e61230751c0be6ce1fe0c1c02029
spgoodman <steve@goodman.net>
2024-06-06 08:49:44 +0100

    Updated webui.sh with defaults for most people

--- webui.sh
+++ webui.sh
@@ -2,22 +2,30 @@
 # Start Web UI
 #
 # Set to limit to particular device
-export CUDA_VISIBLE_DEVICES=1
+#export CUDA_VISIBLE_DEVICES=1
+
 # Set to bind to all IP addresses or leave blank for localhost only
-export GRADIO_SERVER_NAME=0.0.0.0
+#export GRADIO_SERVER_NAME=0.0.0.0
+
 # Set to bind to specific HTTP port, or leave blank for default
-export GRADIO_SERVER_PORT=7360
+#export GRADIO_SERVER_PORT=7360
+
 # Set to true to create sharing link
 #export GRADIO_SHARE=True
+
 # Model Dir - if not set, will download from Huggingface into cache
-export SDAUDIO_MODEL_DIR="../stable-audio-open-1.0"
+#export SDAUDIO_MODEL_DIR="../stable-audio-open-1.0"
+
 # Virtual Environment directory
 export SDAUDIO_VENV_DIR=".venv"
+
+# Startup of webui
 #
 # Change to script directory
 cd "$(dirname "$0")"
 # Create venv
-if [[ ! -d "$SDAUDIO_VENV_DIR" ]] then
+if [[ ! -d "$SDAUDIO_VENV_DIR" ]]
+then
 	mkdir "$SDAUDIO_VENV_DIR"
 	python -m venv "$SDAUDIO_VENV_DIR"
 	source "$SDAUDIO_VENV_DIR/bin/activate"
